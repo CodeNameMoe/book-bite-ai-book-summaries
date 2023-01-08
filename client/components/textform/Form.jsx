5ece8666122601fb1e8d980e864319ebe156ee5f
Mohamed Ali <newmoeali@gmail.com>
2023-01-08 14:04:51 +0000

    added navbar

--- client/components/textform/Form.jsx
+++ client/components/textform/Form.jsx
@@ -20,15 +20,29 @@ function Form() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        {/* <input type="text" placeholder="Enter a book title" /> */}
-        <button type="submit">Submit</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        <h2>BookBite: Your Daily Dose of Literary Summaries</h2>
+        <p>
+          Get quick and easy summaries of your favorite books with our
+          user-friendly website app! Simply type in the title of the book you
+          want to summarize and hit enter. Within seconds, you'll have a concise
+          overview of the main ideas and plot points, saving you time and
+          helping you decide if the book is worth a full read. Try it out now
+          and discover the magic of BookBite!
+        </p>
+      </div>
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          {/* <input type="text" placeholder="Enter a book title" /> */}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
       <div>{response}</div>
     </div>
   );
