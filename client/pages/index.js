a52361a0e4a96dd7fff4af216563e8b5c3510b05
Mohamed Ali <newmoeali@gmail.com>
2023-01-11 12:30:59 +0000

    build bug fix

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Form from "../components/textform/form";
+import Form from "../components/textform/Form.jsx";
 
 export default function Home() {
   return (
